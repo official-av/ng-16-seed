85454c923e1c001cd19f6d4801f604c271678d55
Anmol Vashistha <av@talentrocket.de>
2023-12-28 11:07:23 +0100

    removed scaffold code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,11 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-16-seed');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-16-seed app is running!');
-  });
 });
